25e9ccab20346cc16e51cb56afeba9f3605aa969
Pomin Wu <pomin5@gmail.com>
2016-08-25 19:09:42 +0800

    Fixed load script

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ $ source /srv/localwiki/env/bin/activate
 $ localwiki-manage shell
 >>> import load
 >>> load.run()
+>>> load.ds[0].geom_type
+>>> load.ds[0].srs
+>>> load.ds[0].fields
 ```
--- load.py
+++ load.py
@@ -4,10 +4,16 @@ import os
 from django.contrib.gis.utils import LayerMapping
 from django.contrib.gis.gdal import DataSource
 from localwiki.regions.models import Region, RegionSettings
+from localwiki.pages.models import Page
 from django.contrib.auth.models import User
+from django.utils.translation import activate, get_language
 
+activate('zh')
+
+# town_shp = os.path.join(os.path.dirname(__file__),
+                        # 'village-SHP/Village_NLSC_1050219_UTF8.shp')
 town_shp = os.path.join(os.path.dirname(__file__),
-                        'town-SHP/Town_MOI_1041215_UTF8.shp')
+                        'test-SHP/test_village_UTF8.shp')
 ds = DataSource(town_shp)
 
 admins = []
@@ -19,24 +25,33 @@ for username in ['pm5', 'superbil', 'dongpo']:
         pass
 
 mapping = {
-    'full_name': 'NFULLNAME',
-    'slug': 'SLUG',
+    'full_name': 'FULLNAME',
+    'slug': 'NVILL',
     'geom': 'POLYGON',
 }
 
 
-def clear_region(slug):
-    for i in range(10):
-        regions = Region.objects.filter(slug__startswith=unicode(i))
-        regions.delete()
+def clear_region(region=None, slug=None):
+    if region != None:
+        for page in region.page_set.all():
+            page.delete()
+    elif slug != None:
+        region = Region.objects.get(slug__iexact=slug)
+        for page in region.page_set.all():
+            page.delete()
+    else:
+        Page.objects.all().delete()
 
 
 def run(verbose=True):
-    lm = LayerMapping(
-        Region, town_shp, mapping, transform=True, encoding="utf-8")
-    lm.save(strict=True, verbose=verbose)
+    try:
+        lm = LayerMapping(
+            Region, town_shp, mapping, transform=True, encoding="utf-8")
+        lm.save(strict=True, verbose=verbose)
+    except:
+        print "There are already regions.  Giving up importing regions."
+
     for feat in ds[0]:
-        print(feat.get(mapping['slug']))
         rg = Region.objects.get(slug__iexact=feat.get(mapping['slug']))
         rg.populate_region()
 
