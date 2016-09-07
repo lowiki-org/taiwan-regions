4a925107e3826a3de046ce1b08d843247accfef0
Pomin Wu <pomin5@gmail.com>
2016-09-07 22:07:11 +0800

    Added tags to regions

--- load.py
+++ load.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import os
+import time
 from django.contrib.gis.utils import LayerMapping
 from django.contrib.gis.gdal import DataSource
 from localwiki.regions.models import Region, RegionSettings
 from localwiki.pages.models import Page
+from localwiki.tags.models import Tag, PageTagSet
 from django.contrib.auth.models import User
 from django.utils.translation import activate, get_language
 
@@ -27,7 +29,7 @@ for username in ['pm5', 'superbil', 'dongpo']:
 mapping = {
     'full_name': 'FULLNAME',
     'slug': 'NVILL',
-    'geom': 'POLYGON',
+    'geom': 'POLYGON'
 }
 
 
@@ -50,12 +52,44 @@ def run(verbose=True):
 
     for feat in ds[0]:
         try:
-            rg = Region.objects.get(slug__iexact=feat.get(mapping['slug']))
+            rg = Region.objects.get(slug__startswith=u'tw' + feat.get(mapping['slug']))
             rg.populate_region()
         except:
             print "Alread have pages.  Give up populating this region."
 
-        rs = RegionSettings.objects.get(region__slug__iexact=feat.get(mapping['slug']))
+        tags = []
+        try:
+            tag = Tag.objects.get(name=feat.get('NCITYNAME'))
+        except:
+            tag = Tag(name=feat.get('NCITYNAME'))
+            tag.save()
+        tags.append(tag)
+        try:
+            tag = Tag.objects.get(name=feat.get('NCITYNAME') + feat.get('NTOWNNAME'))
+        except:
+            tag = Tag(name=feat.get('NCITYNAME') + feat.get('NTOWNNAME'))
+            tag.save()
+        tags.append(tag)
+
+        try:
+            fp = Page.objects.get(region=rg, slug__startswith='front')
+        except:
+            fp = Page(region=rg, name='Front Page')
+            fp.save()
+        try:
+            tagset = fp.pagetagset
+        except:
+            tagset = PageTagSet(page=fp, region=rg)
+            tagset.save()
+        for tag in tags:
+            try:
+                tagset.tags.add(tag)
+            except:
+                pass
+
+        rs = RegionSettings.objects.get(region__slug__startswith=u'tw' + feat.get(mapping['slug']))
         rs.admins.add(*admins)
         rs.default_language = u'zh'
         rs.save()
+
+        time.sleep(1)
